023211c5989cc02dfd34ce86308dc4190123843e
Jonathan East <jonathan_east@ymail.com>
2020-12-09 19:43:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const mapToNegativize = function(source) {
   let negNum = [];
   for(let i = 0; i < source.length; i++) {
-    negNum.push(-1 * negNum[i]);
+    negNum.push(-1 * source[i]);
   }
   return negNum;
 }
